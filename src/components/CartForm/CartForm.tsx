f4e41442b1f6c49dd8873542715895a8a960ce39
Salavat F <salmrproperfahr@mail.ru>
2022-07-22 20:40:33 +0300

    done cart form

--- src/components/CartForm/CartForm.tsx
+++ src/components/CartForm/CartForm.tsx
@@ -5,6 +5,9 @@ import userIcon from "../../assets/svg/user.svg";
 import phoneIcon from "../../assets/svg/phone.svg";
 import emailIcon from "../../assets/svg/email.svg";
 import caseIcon from "../../assets/svg/briefcase.svg";
+import cartIcon from "../../assets/svg/cart-white-2.svg";
+import docIcon from "../../assets/svg/file-text.svg";
+import downloadIcon from "../../assets/svg/download.svg";
 
 const CartForm: React.FC = () => {
     const inputs = [
@@ -43,6 +46,28 @@ const CartForm: React.FC = () => {
                         />
                     );
                 })}
+
+                <div className="form__sum-wrapper">
+                    <div className="form__sum-title">Итого</div>
+                    <div className="form__sum">8 499 руб.</div>
+                </div>
+                <div className="cart__form-actions">
+                    <button className="cart__btn cart__btn--buy">
+                        <img src={cartIcon} alt="" />
+                        Оформить заказ
+                    </button>
+                    <button className="cart__btn cart__btn--offer">
+                        <div className="offer__btn-wrapper">
+                            <div className="offer__btn-doc">
+                                <img src={docIcon} alt="" />
+                            </div>
+                            <div className="offer__btn-download">
+                                <img src={downloadIcon} alt="" />
+                            </div>
+                            Коммерческое предложение
+                        </div>
+                    </button>
+                </div>
             </form>
         </div>
     );
